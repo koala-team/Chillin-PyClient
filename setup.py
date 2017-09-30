1f64954e873338b2fb874eeaf6a607815f4834ae
AminHP <mdan.hagh@gmail.com>
2017-09-30 06:38:44 +0330

    Change version to 1.0.1

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='chillin-client',
-    version='1.0.0',
+    version='1.0.1',
     description='Chillin AI Game Framework (Python Client)',
     long_description='',
-    author='k04la',
+    author='Koala',
     author_email='mdan.hagh@gmail.com',
-    url='https://github.com/k04la/Chillin-PyClient',
+    url='https://github.com/koala-team/Chillin-PyClient',
     keywords='ai game framework chillin',
 
     classifiers=[
         'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
@@ -22,10 +22,10 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks'
     ],
 
-    license='MIT',
+    license='AGPL License, Version 3.0',
 
     install_requires=[
-        'koala-serializer==0.5.0',
+        'koala-serializer==0.5.1',
         'configparser==3.5.0',
         'enum34==1.1.6'
     ],
