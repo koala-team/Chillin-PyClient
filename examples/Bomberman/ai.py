fbe779b22a3a99151a4b1da5a8dbbc7f4ab28d5f
AminHP <mdan.hagh@gmail.com>
2017-10-05 03:04:49 +0330

    Clean Bomberman codes

--- examples/Bomberman/ai.py
+++ examples/Bomberman/ai.py
@@ -11,20 +11,22 @@ from ks.commands import Move, Bomb, EDir
 from ks.models import ECellType
 
 
-class MyRealtimeAI(RealtimeAI):
+class AI(RealtimeAI):
 
     def __init__(self, world):
-        super(RealtimeAI, self).__init__(world)
+        super(AI, self).__init__(world)
 
         self.directions = [EDir.Top, EDir.Right, EDir.Bottom, EDir.Left]
 
 
     def decide(self):
         print ('decide')
+
         cmd_rand = random.uniform(0.0, 1.0)
         if cmd_rand < 0.9: # Move
             bomberman = self.world.bombermans[self.my_side]
             valid_directions = []
+
             if self.world.board[bomberman.y - 1][bomberman.x].type == ECellType.Empty:
                 valid_directions.append(EDir.Top)
             if self.world.board[bomberman.y][bomberman.x + 1].type == ECellType.Empty:
@@ -38,7 +40,3 @@ class MyRealtimeAI(RealtimeAI):
                 self.send_command(Move(direction=valid_directions[random.randrange(0, len(valid_directions))]))
         else: # Bomb
             self.send_command(Bomb())
-            
-
-
-AI = MyRealtimeAI
--- examples/XO/ai.py
+++ examples/XO/ai.py
@@ -8,7 +8,7 @@ from ks.commands import Place
 from ks.models import ECell
 
 
-class MyTurnbasedAI(TurnbasedAI):
+class AI(TurnbasedAI):
 
     def decide(self):
         for i in range(0, 3):
@@ -16,6 +16,3 @@ class MyTurnbasedAI(TurnbasedAI):
                 if self.world.board[i][j] == ECell.Empty:
                     self.send_command(Place(x=j, y=i))
                     return
-
-
-AI = MyTurnbasedAI
