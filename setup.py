c9988108f5e4f74e0764aa6112cad79a0acc4b41
AminHP <mdan.hagh@gmail.com>
2017-10-05 00:56:42 +0330

    Change version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chillin-client',
-    version='1.0.1',
+    version='1.1.0',
     description='Chillin AI Game Framework (Python Client)',
     long_description='',
     author='Koala',
